334e484dd635c876a2dd72e1600d3c2ae106332b
StillFrost75 <128876738+StillFrost75@users.noreply.github.com>
2023-05-06 09:18:07 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pelajari React selamanya
         </a>
       </header>
     </div>
